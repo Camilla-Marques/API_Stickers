0a43ed2bd79802e4f09cd43229afdcce901312d1
Camilla-Marques <camilla.93marques@gmail.com>
2022-07-20 21:42:54 -0300

    Começo da api acessando a api do imdb.

---
+++ api_stickers/src/App.java
@@ -0,0 +1,24 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandler;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // executar conexão HTTP e buscar os top 250 filmes
+        String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
+        URI endereco = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
+
+        // vamos extrair só os dados que realmente interessam (título, poster, classificação)
+
+        // exibir e manipular os dados
+
+    }
+}
